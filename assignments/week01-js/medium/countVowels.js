58665c8732a8f26a516e0b40fc5bff2deb1d9b2d
Ali_jan <hassanfarid667@gmail.com>
2025-04-17 00:17:51 +0500

    first comment

---
+++ assignments/week01-js/medium/countVowels.js
@@ -0,0 +1,31 @@
+/*
+  Implement a function `countVowels` that takes a string as an argument and returns the number of vowels in the string.
+  Note: Consider both uppercase and lowercase vowels ('a', 'e', 'i', 'o', 'u').
+
+  Once you've implemented the logic, test your code by running
+*/
+
+function countVowels(str) {
+  
+  str = str.toLowerCase()
+
+  let vowels = "aeiou"
+
+  let vowelsLength = 0
+
+  for (let i = 0; i < str.length; i++){
+
+    if (vowels.includes(str[i])){
+      vowelsLength++
+    }
+  }
+  return vowelsLength
+}
+let res = countVowels("programming")
+console.log(res)
+
+console.log(countVowels("Python"))
+console.log(countVowels("rhythm"))
+console.log(countVowels("I my name is Hassan ALi"))
+
+module.exports = countVowels;
