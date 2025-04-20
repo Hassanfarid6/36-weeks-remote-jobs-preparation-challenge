8bc24e90c3c4f61e640135c5ea42ea4c62d2ed59
Ali_jan <hassanfarid667@gmail.com>
2025-04-20 09:04:08 +0500

    Todo is added in hard task

--- assignments/week01-js/hard/todo-list.js
+++ assignments/week01-js/hard/todo-list.js
@@ -12,6 +12,68 @@
 
 class Todo {
 
+  constructor(){
+    this.todos = []
+  }
+
+  add(todo){
+    this.todos.push(todo)
+    return this
+  }
+
+  remove(index){
+       if(index >= 0 && index < this.todos.length){
+        this.todos.splice(index, 1)
+      }
+      return this
+      }
+  update(index, updatedTodo){
+    if (index >= 0 && index < this.todos.length){
+      this.todos[index] = updatedTodo
+    }
+    return this
+  }
+
+  getAll(){
+    return this.todos
+  }
+  
+  get(index){
+    if(index >= 0 && index < this.todos.length){
+      this.todos[index]
+      return this
+      }
+    return null
+  }
+  clear(){
+    this.todos = []
+    return this 
+  }
+
 }
+
+const myTodos = new Todo();
+myTodos.add("Buy milk").add("Walk dog");
+
+console.log(myTodos.getAll()); // Should print: ["Buy milk", "Walk dog"]
+
+let re = myTodos.add('Buy groceries').add('Read book').getAll(); // ['Buy groceries', 'Read book']
+let res = myTodos.get(0); // 'Buy groceries'
+let ree = myTodos.update(1, 'Finish book').getAll(); // ['Buy groceries', 'Finish book']
+let rem = myTodos.remove(0).getAll(); // ['Finish book']
+let clr = myTodos.clear().getAll(); // []
+let err;
+try {
+  myTodos.get(0); // Throws: Error: Invalid index
+} catch (e) {
+  err = e.message;
+}
+
+console.log(re); // ['Buy groceries', 'Read book']
+console.log(res); // 'Buy groceries'
+console.log(ree); // ['Buy groceries', 'Finish book']
+console.log(rem); // ['Finish book']
+console.log(clr); // []
+console.log(err); //
 
 module.exports = Todo;
